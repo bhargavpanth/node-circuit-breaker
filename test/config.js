bf6780dde5e4d44b6a8ddbd1a7b8d2be9cfe065f
Bhargav Panth <bhargavrpanth@gmail.com>
2018-04-13 20:16:44 +0100

    init structs

---
+++ test/config.js
@@ -0,0 +1,10 @@
+var mocha = require('mocha');
+var chai = require('chai');
+var assert = chai.assert;
+
+describe('perform sanity checks on dev environment', () => {
+	it('check if NODE_ENV is set to dev', () => {
+		// console.log(process.env.NODE_ENV);
+		assert.equal(process.env.NODE_ENV, undefined);
+	});
+});
