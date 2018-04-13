02e79daaaf1dcb40340c3ab53491679d90567b20
Bhargav Panth <bhargavrpanth@gmail.com>
2018-04-13 20:59:31 +0100

    add babelrc file

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@
 Capture response and check for a timeout
 */
 
+import fetch from 'node-fetch';
+
 let env = process.env.NODE_ENV = 'test';
 
+export default class circuit_breaker{
+	// default timeout wait time is set to 10 unless explicitly mentioned
+	handle(request, url, timeout = 10){
+		new Promise( (resolve, reject) => {
+			fetch(url).then()
+		});
+	}
+}
