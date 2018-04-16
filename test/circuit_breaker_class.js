20d21455a5201d0a2b0d3e5fbc61ae9639f8c199
Bhargav Panth <bhargavrpanth@gmail.com>
2018-04-16 17:03:10 +0100

    import es6 class in es5 after babel

--- test/circuit_breaker_class.js
+++ test/circuit_breaker_class.js
@@ -1,6 +1,11 @@
 var mocha = require('mocha');
 var chai = require('chai');
 var assert = chai.assert;
+var cb = require('../build/circuit_breaker').default;
+
+console.log(Object.keys(cb));
+console.log(cb);
+console.log(cb.circuit_breaker('http://github.com', 10));
 
 describe('sanity checks for circuit_breaker class', () => {
 	it('should instantiate an object of class circuit breaker passing all 2 params', () => {
