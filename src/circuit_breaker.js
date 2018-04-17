c58fb43e64b77c98a8738385e743f1860bd10258
Bhargav Panth <bhargavrpanth@gmail.com>
2018-04-17 20:43:11 +0100

    commit before format

--- src/circuit_breaker.js
+++ src/circuit_breaker.js
@@ -11,7 +11,7 @@ import request from 'request';
 class circuit_breaker{
 
 	// default wait timeout is set to 10 unless explicitly mentioned
-	constructor(url, timeout = 10){
+	constructor(url, timeout = 10000){
 		this.url = url;
 		this.timeout = timeout;
 	};
@@ -34,17 +34,18 @@ class circuit_breaker{
 		});
 		// hit a request and start a setTimeout function
 		
-		Promise.race([req, timeout]).then( () => {
-			console.log(`Response`);
-		}).catch( () => {
-			console.log(`Unable to fetch the request on time - setting circuit breaker`);
+		Promise.race([req, timeout]).then( (val) => {
+			console.log(`Response ${val}`);
 		});
+		// .catch( () => {
+		// 	console.log(`Unable to fetch the request on time - setting circuit breaker`);
+		// });
 	
 	};
 
 };
 
-let test = new circuit_breaker('https://github.com', 1000)
+let test = new circuit_breaker('https://github.com', 10000)
 test.handle();
 
 export default circuit_breaker;
