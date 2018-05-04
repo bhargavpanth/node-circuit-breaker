b7e0a261ff6c2190353791f0214ae427c7e57bd3
Bhargav Panth <bhargavrpanth@gmail.com>
2018-05-04 21:36:58 +0100

    check if circle ci runs

--- test/circuit_breaker_class.js
+++ test/circuit_breaker_class.js
@@ -1,7 +1,7 @@
 var mocha = require('mocha');
 var chai = require('chai');
 var assert = chai.assert;
-var cb = require('../build/circuit_breaker');
+var cb = require('../build/circuit_breaker.js');
 
 // Object.getOwnPropertyNames();
 // console.log(Object.keys(cb));
