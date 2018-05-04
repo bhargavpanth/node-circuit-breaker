4b940252346d2064c5e857b9a815fd852f4b186d
Bhargav Panth <bhargavrpanth@gmail.com>
2018-05-04 20:04:47 +0100

    mismatch in package.json

--- src/circuit_breaker.js
+++ src/circuit_breaker.js
@@ -4,7 +4,6 @@ Capture response and check for a timeout
 
 import fetch from 'node-fetch';
 
-import request from 'request';
 
 // let env = process.env.NODE_ENV = 'test';
 
@@ -19,24 +18,33 @@ class circuit_breaker{
 	handle() {
 		
 		let req = new Promise( (resolve, reject) => {
+			
 			fetch(this.url).then((data) => {
 				resolve(data);
-			}).catch((err) => {
+			})
+
+			.catch((err) => {
 				reject(err);
 			});
+
 		});
 
+		
 		let timeout = new Promise( (resolve, reject) => {
+
 			let time = setTimeout(() => {
 				clearTimeout(time);
 				reject('request timed out at ' + this.timeout + ' seconds');
+				/* close the circuit and set a flag to indicate the service is down */
 			});
+
 		});
-		// hit a request and start a setTimeout function
 		
+		// hit a request and start a setTimeout function
 		Promise.race([req, timeout]).then( (val) => {
 			console.log(`Response ${val}`);
 		});
+
 		// .catch( () => {
 		// 	console.log(`Unable to fetch the request on time - setting circuit breaker`);
 		// });
--- test/circuit_breaker_class.js
+++ test/circuit_breaker_class.js
@@ -3,15 +3,17 @@ var chai = require('chai');
 var assert = chai.assert;
 var cb = require('../build/circuit_breaker');
 
-// Object.getOwnPropertyNames()
+// Object.getOwnPropertyNames();
 // console.log(Object.keys(cb));
-console.log(cb);
-console.log(cb('http://github.com', 10));
+
 
 describe('sanity checks for circuit_breaker class', () => {
+
 	it('should instantiate an object of class circuit breaker passing all 2 params', () => {
 		var test = new circuit('http://github.com/bhargavpanth', 10);
-		console.log(test);
 		assert.equal(1,1);
 	});
+
+	// it('', () => {});
+
 });
